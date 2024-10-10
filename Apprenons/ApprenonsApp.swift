7d3c00c66f6cc47997cdafaa1383d9bfa2b6d01c
algabl <ablack00@byu.edu>
2024-10-10 08:58:03 -0600

    Initial Commit

---
+++ Apprenons/ApprenonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApprenonsApp.swift
+//  Apprenons
+//
+//  Created by Alexander Black on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ApprenonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
